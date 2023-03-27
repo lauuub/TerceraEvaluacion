5061c0d8b3d20dea367204ced1980337c3ac0937
Laura Blasco <113889507+lauuub@users.noreply.github.com>
2023-03-27 09:52:52 +0200

    Add files via upload

--- Cuenta.java
+++ Cuenta.java
@@ -1,5 +1,36 @@
 package EjerciciosClases;
 
 public class Cuenta {
-
+	private String titular;
+	private double cantidad;
+	//priemer constructor
+	public Cuenta(String titular, double cantidad) {
+		//titular=nuevoTitular;
+		//cantidad=nuevaCantidad;
+		this.titular=titular;
+		this.cantidad=cantidad;
+	}
+	//segundo constructor
+	public Cuenta(String titular) {
+		this.titular=titular;
+	}
+	//métodos 
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	public double getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	@Override
+	public String toString() {
+		return "Cuenta [titular=" + titular + ", cantidad=" + cantidad + "]";
+	}
+	
+	
 }
